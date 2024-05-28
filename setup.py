ba7c126a56e7d03a11682ad75f27a19b301d6e6b
Rushi908 <99402463+Rushi908@users.noreply.github.com>
2024-05-28 18:32:46 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Rushikesh Shinde',
+    author_email= 'rushikeshshinde20@yahoo.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
